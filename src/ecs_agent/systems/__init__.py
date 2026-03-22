14320bb38c36a9fa284278965ea8228f7a960774
roy1994 <dev.mancitrus@outlook.com>
2026-03-23 02:47:33 +0800

    refactor(prompts): add user prompt normalization system

--- src/ecs_agent/systems/__init__.py
+++ src/ecs_agent/systems/__init__.py
@@ -15,6 +15,9 @@ from ecs_agent.systems.tool_approval import ToolApprovalSystem
 from ecs_agent.systems.tree_search import TreeSearchSystem
 from ecs_agent.systems.rag import RAGSystem
 from ecs_agent.systems.user_input import UserInputSystem
+from ecs_agent.systems.user_prompt_normalization_system import (
+    UserPromptNormalizationSystem,
+)
 
 __all__ = [
     "CheckpointSystem",
@@ -34,4 +37,5 @@ __all__ = [
     "ToolExecutionSystem",
     "TreeSearchSystem",
     "UserInputSystem",
+    "UserPromptNormalizationSystem",
 ]
